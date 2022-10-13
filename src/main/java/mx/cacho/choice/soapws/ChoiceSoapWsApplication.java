a7cff7fd805340c3ace2996e49e6e0a4cf2ca48a
Allan Herrera <allan.herrera@encora.com>
2022-10-13 18:42:48 -0500

    Initial commit

---
+++ src/main/java/mx/cacho/choice/soapws/ChoiceSoapWsApplication.java
@@ -0,0 +1,13 @@
+package mx.cacho.choice.soapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceSoapWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoiceSoapWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/cacho/choice/soapws/ChoiceSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.cacho.choice.soapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceSoapWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
