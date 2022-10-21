8171c01e758d77ff3a61ca1776e51c738a939e12
Allan Herrera <allan.herrera@encora.com>
2022-10-21 16:27:59 -0500

    feat: Add amenities catalog TODO: generic descriptions

---
+++ src/main/resources/db/migration/V1.1__choice_amenities_dml.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    amenities (name, description)
+VALUES
+    ('Pool','Awesome pool fun for the whole family'),
+    ('Wifi','Great Wifi coverage indoors & outdoors'),
+    ('Pet Friendly','All pets are welcome!'),
+    ('Free Parking','Do not worry about parking!'),
+    ('Free Breakfast','Chilaquiles are on us!'),
+    ('Gym','Dont skip leg day'),
+    ('High-speed internet','1GBPS Internet'),
+    ('Arcade','Can you beat the high scores?'),
+    ('Free Beer','You heard it right. Some conditions apply.'),
+    ('?','?????');
